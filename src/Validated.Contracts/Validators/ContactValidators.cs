d80e69727b71d42cdfc95b0ed2dc8ce86b779c96
Paul Kent <paulandrewkent@hotmail.com>
2026-03-30 13:47:13 +0100

    Noticed wrong regex pattern due to copy paste

--- src/Validated.Contracts/Validators/ContactValidators.cs
+++ src/Validated.Contracts/Validators/ContactValidators.cs
@@ -26,7 +26,7 @@ public static class ContactValidators
 
         GivenNameValidator   = MemberValidators.CreateStringRegexValidator(@"^(?=.{2,50}$)[A-Z]+['\- ]?[A-Za-z]*['\- ]?[A-Za-z]+$", "GivenName", "First name", "Must start with a capital letter and be between 2 and 50 characters in length");
 
-        FamilyNameValidator  = MemberValidators.CreateStringRegexValidator(@"^[A-Z]+['\- ]?[A-Za-z]*['\- ]?[A-Za-z]*$", "FamilyName", "Surname", "Must start with a capital letter")
+        FamilyNameValidator  = MemberValidators.CreateStringRegexValidator(@"^[A-Z].*$", "FamilyName", "Surname", "Must start with a capital letter")
                                 .AndThen(MemberValidators.CreateStringLengthValidator(2, 50, "FamilyName", "Surname", "Must be between 2 and 50 characters in length"));
 
         AgeValidator         = MemberValidators.CreateRangeValidator(25, 50, "Age", "Age", "Must be between 25 and 50");
